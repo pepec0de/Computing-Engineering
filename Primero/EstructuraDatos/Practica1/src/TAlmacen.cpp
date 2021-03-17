6bc8339aa5345042a30ab9d17c8c8cff350af2de
Pepe González <pepeglezabad@gmail.com>
2021-03-17 20:27:54 +0100

    Terminando TAlmacen

--- Primero/EstructuraDatos/Practica1/src/TAlmacen.cpp
+++ Primero/EstructuraDatos/Practica1/src/TAlmacen.cpp
@@ -2,12 +2,101 @@
 
 using namespace std;
 
-TAlmacen::TAlmacen()
-{
+TAlmacen::TAlmacen() {
     //ctor
+    strcpy(Nombre, "");
+    strcpy(Direccion, "");
+    NProduc = -1;
+    // Inicializamos el fstream
+    /* Flags:
+     * ios::binary -> modo archivo binario
+     * ios::out -> modo escritura
+     * ios::in -> modo lectura
+     * ios::trunc -> crear el archivo de nuevo
+     */
+    FicheProductos.open("Almacen.dat", ios::binary | ios::out | ios::in);
+    if (FicheProductos.fail()) {
+        // El archivo no existe, lo creamos
+        FicheProductos.close();
+        FicheProductos.clear();
+        FicheProductos.open("Almacen.dat", ios::binary | ios::out);
+        FicheProductos.close();
+        FicheProductos.clear();
+        FicheProductos.open("Almacen.dat", ios::binary | ios::out | ios::in);
+    }
+    FicheProductos.close();
 }
 
-TAlmacen::~TAlmacen()
-{
+TAlmacen::~TAlmacen() {
     //dtor
+}
+
+// Devuelve los atributos nombre y dirección por parámetro.
+void TAlmacen::DatosAlmacen(Cadena pNombAlmacen, Cadena pDirAlmacen) {
+    strcpy(pNombAlmacen, Nombre);
+    strcpy(pDirAlmacen, Direccion);
+}
+
+// Crea un fichero binario vacío con el nombre pasado por parámetro. Crea la cabecera del fichero
+// y lo deja abierto para su utilización. Devuelve true si se ha creado.
+bool TAlmacen::CrearAlmacen(Cadena pNomFiche) {
+    fstream nuevoFiche;
+    nuevoFiche.open(pNomFiche, ios::binary | ios::out | ios::in);
+    if (!nuevoFiche.fail()) {
+        return true;
+    }
+    return false;
+}
+
+// Igual que el método anterior, pero actualizando los atributos nombre y dirección con los valores
+// pasados por parámetro. Devuelve true si ha podido crear el fichero.
+bool TAlmacen::CrearAlmacen(Cadena pNombAlmacen, Cadena pDirAlmacen, Cadena pNomFiche) {
+    strcpy(Nombre, pNombAlmacen);
+    strcpy(Direccion, pDirAlmacen);
+
+    return CrearAlmacen(pNomFiche);
+}
+
+// Abre un fichero y actualiza los atributos de la clase con los datos de cabecera del fichero y lo
+// lo deja abierto. No se puede abrir un fichero si previamente el almacén está abierto. Devuelve true
+// si ha podido abrir el fichero.
+// 0 -> Numero Productos
+// 1 -> Nombre
+// 2 -> Direccion
+bool TAlmacen::AbrirAlmacen(Cadena pNomFiche) {
+    if (EstaAbierto()) return false;
+
+    FicheProductos.open(pNomFiche, ios::binary | ios::out | ios::in);
+    if (!FicheProductos.fail()) {
+        FicheProductos.seekg(0);
+        FicheProductos.read((char*) &NProduc, sizeof(int));
+        FicheProductos.read((char*) &Nombre, sizeof(Cadena));
+        FicheProductos.read((char*) &Direccion, sizeof(Cadena));
+        return true;
+    }
+
+    return false;
+}
+
+// Cierra el fichero e inicializa los atributos a valores iniciales. Devuelve true si se ha cerrado el
+// almacén.
+bool TAlmacen::CerrarAlmacen() {
+    NProduc = -1;
+    FicheProductos.close();
+    FicheProductos.clear();
+
+    return !FicheProductos.is_open();
+}
+
+bool TAlmacen::EstaAbierto() {
+    return NProduc != -1;
+}
+
+int TAlmacen::NProductos() {
+    return NProduc;
+}
+
+int TAlmacen::BuscarProducto(Cadena pCodProd) {
+
+    return 0;
 }
--- Primero/EstructuraDatos/Practica1/src/TTienda.cpp
+++ Primero/EstructuraDatos/Practica1/src/TTienda.cpp
@@ -1,11 +1,103 @@
 #include "TTienda.h"
 
+// Metodo privado
+void TTienda::OrdenarProductos() {
+
+}
+
 TTienda::TTienda()
 {
     //ctor
+
 }
 
 TTienda::~TTienda()
 {
     //dtor
 }
+
+//Devuelve los atributos nombre y dirección por parámetro.
+void TTienda::DatosTienda(Cadena pNombTienda, Cadena pDirTienda) {
+
+}
+
+//Crea un fichero binario vacío con el nombre pasado por parámetro e inicializa los atributos nombre y
+//dirección mediante los parámetros y a continuación lo cerrará. Devolverá true si ha podido crear el
+//fichero.
+bool TTienda::CrearTienda(Cadena pNombTienda, Cadena pDirTienda, Cadena pNomFiche) {
+    return false;
+}
+
+//Abre un fichero y lo carga a memoria. Si ya había un fichero previamente cargado, guardará los datos
+//de la tienda y procederá a cargar el nuevo fichero. Si el fichero es el mismo que el que está en
+//memoria, eliminará los datos y procederá a cargar nuevamente los datos del fichero. Devolverá true
+//si se ha podido cargar el fichero.
+bool TTienda::AbrirTienda(Cadena pNomFiche) {
+    return false;
+}
+
+//Vuelca los datos de la memoria al fichero. Devolverá true si se han guardado los datos.
+bool TTienda::GuardarTienda() {
+    return false;
+}
+
+// Guarda los datos de la memoria en el fichero y borra todos los atributos del objeto. Devuelve true
+// si se ha podido guardar los datos.
+bool TTienda::CerrarTienda() {
+    return false;
+}
+
+// Devuelve true si la tienda está abierta.
+bool TTienda::EstaAbierta() {
+    return false;
+}
+
+// Devuelve el número de estantes de la tienda.
+int TTienda::NoEstantes() {
+    return 0;
+}
+
+// Dado un código de estante, devuelve la posición dentro del vector donde se encuentra. Si no lo
+// encuentra devuelve -1.
+int TTienda::BuscarEstante(int pCodEstante) {
+    return 0;
+}
+
+// Dado la posición el estante que está en la posición indicada por parámetro.
+TEstante TTienda::ObtenerEstante(int pPos) {
+    TEstante resultado;
+    return resultado;
+}
+
+// Añade un estante nuevo al vector siempre que el estante no esté previamente almacenado en memoria.
+// El vector de estantes debe siempre estar ordenado. Devolverá true si se ha añadido el estante.
+bool TTienda::AnadirEstante(TEstante pEstante) {
+    return false;
+}
+
+// Dado la posición de un estante lo borra desplazando el resto de estantes una posición hacia abajo.
+// Se debe verificar previamente que la posición sea correcta. Devuelve true si se ha eliminado el
+// estante.
+bool TTienda::EliminarEstante(int pPos) {
+    return false;
+}
+
+// Dada la posición de un estante, lo actualiza con los datos pasados por parámetros. Se debe verificar
+// previamente que la posición sea correcta Devuelve true si se actualiza el estante.
+bool TTienda::ActualizarEstante(int pPos, TEstante pEstante) {
+    return false;
+}
+
+// Dada la posición de un estante y un producto del almacén, actualizará el número de productos del
+// estante a su máxima capacidad si hay suficientes unidades en el producto, en caso contrario se
+// añadirán al estante la totalidad de unidades que estén en el producto del almacén. El mismo número
+// de unidades añadidas en el estante deben reducirse del producto. Se debe verificar previamente que
+// la posición sea correcta. El método devuelve:
+//  0 si la posición es incorrecta.
+//  1 si se ha repuesto unidades hasta llegar a la capacidad máxima del estante.
+//  2 si no se ha completado el estante al completo.
+int TTienda::ReponerEstante(int pPos, TProducto &pProduc) {
+    return 0;
+}
+
+
