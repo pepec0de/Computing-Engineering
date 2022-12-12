834e7941bee2b9457ee1b9143bd8ff1bc6f236d8
Pepe <pepeglezabad@gmail.com>
2022-12-12 16:50:20 +0100

    started practice 5 ISDD

---
+++ Tercero/InformationSystems/Practice5/src/main/java/isdd/practice5/Application/Practice5.java
@@ -0,0 +1,12 @@
+package isdd.practice5.Application;
+
+/**
+ *
+ * @author Pepe
+ */
+public class Practice5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Tercero/InformationSystems/Practice5/src/main/java/isdd/practice5/View/DialogView.java
@@ -0,0 +1,26 @@
+package isdd.practice5.View;
+
+import java.awt.TrayIcon;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Pepe
+ */
+public class DialogView {
+
+    public void show(int type, String msg) {
+        switch (type) {
+            case -1:
+                JOptionPane.showMessageDialog(null, msg, "ERROR", TrayIcon.MessageType.ERROR.ordinal());
+                break;
+            
+            default:
+                JOptionPane.showMessageDialog(null, msg, "WARNING", TrayIcon.MessageType.INFO.ordinal());
+        }
+    }
+    
+    public int confirmWarning(String msg) {
+        return JOptionPane.showConfirmDialog(null, msg, "WARNING", 0, 2);
+    }
+}
