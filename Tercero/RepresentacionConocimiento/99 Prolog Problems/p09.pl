d96b8c4813b5d9970895c591b9955d77ec62602c
Pepe González <pepeglezabad@gmail.com>
2023-02-23 11:20:46 +0100

    Update p09.pl
    still no sol

--- Tercero/RepresentacionConocimiento/99 Prolog Problems/p09.pl
+++ Tercero/RepresentacionConocimiento/99 Prolog Problems/p09.pl
@@ -3,11 +3,19 @@ Pack consecutive duplicates of list elements into sublists.
 If a list contains repeated elements they should be placed in separate sublists.
 
     Example:
-    ?- pack([a,a,a,a,b,c,c,a,a,d,e,e,e,e],X).
+    ?- my_pack([a,a,a,a,b,c,c,a,a,d,e,e,e,e],X).
     X = [[a,a,a,a],[b],[c,c],[a,a],[d],[e,e,e,e]]
 */
 
 my_pack([X], [[X]]).
-my_pack([H | Resto], [Lista | X]) :- hacerLista(H, Resto, Lista, Resto2), my_pack(Resto2, X).
+my_pack([H, H | Resto], X) :- my_pack([H | Resto], X).
 
-hacerLista(H, [H | Resto], [H | Lista], Resto) :- hacerLista(H, Resto, Lista, Resto).
+my_pack([H1, H2 | Resto], [Lista2 | X]) :- H1 \= H2, insertarEenLista(H1, Lista, Lista2), my_pack([H2 | Resto], [Lista | X]).
+
+
+/*
+	hacerLista(+E, +N, -L)
+	
+*/
+hacerLista(E, 0, []).
+hacerLista(E, N, [E | L]) :- N1 is N - 1, hacerLista(E, N1, L).
