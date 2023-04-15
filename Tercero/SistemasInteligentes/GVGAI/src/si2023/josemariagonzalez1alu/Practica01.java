7dc37c5f99f61d3c116697564e29125d41879f73
Pepe <pepeglezabad@gmail.com>
2023-04-15 21:02:43 +0200

    si - gvgai main files

--- Tercero/SistemasInteligentes/GVGAI/src/si2023/josemariagonzalez1alu/Practica01.java
+++ Tercero/SistemasInteligentes/GVGAI/src/si2023/josemariagonzalez1alu/Practica01.java
@@ -1,9 +1,58 @@
 package si2023.josemariagonzalez1alu;
 
+import java.util.Random;
+import tools.Utils;
+import tracks.ArcadeMachine;
+
 public class Practica01 {
 
-	public Practica01() {
-		// TODO Auto-generated constructor stub
-	}
+    public static void main(String[] args) {
+    	
+    	String p = "si2023.josemariagonzalez1alu.p01.Agente89";
+    	String pintar = "si2023.josemariagonzalez.p03.agente89.Agente89_Pintar";
+    	String agente = p;
 
+		//Load available games
+		String spGamesCollection =  "examples/all_games_sp.csv";
+		String[][] games = Utils.readGames(spGamesCollection);
+		Random rand = new Random(System.currentTimeMillis());
+		
+		//Game settings
+//		boolean visuals = false;
+				
+		// Game and level to play
+		int gameIdx  = 89;
+		int levelIdx = 4; // level names from 0 to 4 (game_lvlN.txt).
+		
+		String gameName = games[gameIdx][1];
+		String game = games[gameIdx][0];
+		String level1 = game.replace(gameName, gameName + "_lvl" + levelIdx);
+
+		// 1. This starts a game, in a level, played by a human.
+//		ArcadeMachine.playOneGame(game, level1, null, rand.nextInt());
+		
+		
+		boolean rate = true;
+		int seed = rand.nextInt();
+		double time = 0.0;
+		double score = 0.0;
+		int n = 100;
+		int winrate = 0;
+		for (int i = 0; i < n; i++) {
+			seed = rand.nextInt();
+			double[] res = ArcadeMachine.runOneGame(game, level1, false, agente, null, seed, 0);
+			System.out.println("Win: " + res[0] + " Score: " + res[1] + " Time: " + res[2]);
+			if (res[0] == 1) {
+				winrate++;
+				score += res[1];
+				time += res[2];
+			}
+		}
+		System.out.println("Win rate: " + (double)winrate/(double)n);
+		System.out.println("Score: " + (double)score/(double)winrate + "\nTime: " + (double)time/(double)winrate);
+		System.out.println();
+		
+		System.exit(0);
+
+    }
 }
---
+++ Tercero/SistemasInteligentes/GVGAI/src/si2023/josemariagonzalez1alu/Practica03.java
@@ -0,0 +1,70 @@
+package si2023.josemariagonzalez1alu;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+import tools.Utils;
+import tracks.ArcadeMachine;
+
+public class Practica03 {
+
+    public static void main(String[] args) {
+
+    	Random rand = new Random(System.currentTimeMillis());
+    	
+		String agente = "si2023.josemariagonzalez1alu.p03.Agente89";
+		
+		//Load available games
+		String spGamesCollection =  "examples/all_games_sp.csv";
+		String[][] games = Utils.readGames(spGamesCollection);
+
+		// Game settings
+		int seed;
+		
+		// Game and level to play
+		int gameIdx  = 89;
+		int levelIdx = 4; // level names from 0 to 4 (game_lvlN.txt).
+		
+		String gameName = games[gameIdx][1];
+		String game = games[gameIdx][0];
+		String level1 = game.replace(gameName, gameName + "_lvl" + levelIdx);
+
+		
+		// 1. This starts a game, in a level, played by a human.
+		//ArcadeMachine.playOneGame(game, level1, null, seed);
+		
+		System.out.print("Calcular win rate? (S/n): ");
+		Scanner sc = new Scanner(System.in);
+		String opc = sc.next();
+		
+		boolean rate = opc.toLowerCase().equals("s");
+		if (rate) {
+			ArrayList<Integer> seedsMalas = new ArrayList<>();
+			int n = 100;
+			int winrate = 0;
+			for (int i = 0; i < n; i++) {
+				seed = rand.nextInt();
+				double[] res = ArcadeMachine.runOneGame(game, level1, false, agente, null, seed, 0);
+				System.out.println("Win: " + res[0] + " Score: " + res[1] + " Time: " + res[2]);
+				if (res[0] == 1)
+					winrate++;
+				else
+					seedsMalas.add(seed);
+			}
+			System.out.println("Win rate: " + (double)winrate/(double)n);
+			System.out.print("Seeds perdidas: ");
+			for (int s : seedsMalas)
+				System.out.print(s + ", ");
+			System.out.println();
+		} else {
+			// 2. This plays a game in a level by the controller.
+			seed = rand.nextInt();
+			ArcadeMachine.runOneGame(game, level1, true, agente, null, seed, 0);
+		}
+				
+
+		System.exit(0);
+
+    }
+}
