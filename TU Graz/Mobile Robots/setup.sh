12f35db2a301569e2b04eff3c2eeb6e0d5fa3034
Pepe <pepeglezabad@gmail.com>
2023-11-11 15:01:24 +0100

    forced apt-get yes/no prompt

--- TU Graz/Mobile Robots/setup.sh
+++ TU Graz/Mobile Robots/setup.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
-sudo apt get update
+sudo apt-get update
 echo "BASIC PACKAGES"
-sudo apt install mlocate
-sudo apt install git
+sudo apt install -y mlocate
+sudo apt install -y git
 echo "ROS INSTALLATION"
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt install curl
+sudo apt install -y curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update
-sudo apt install ros-noetic-desktop-full
+sudo apt install -y ros-noetic-desktop-full
 echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
-sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
-sudo apt install python3-rosdep
+sudo apt install -y python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+sudo apt install -y python3-rosdep
 sudo rosdep init
 rosdep update
 echo "CATKIN INSTALLATION"
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main" > /etc/apt/sources.list.d/ros-latest.list'
 wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install python3-catkin-tools
+sudo apt-get install -y python3-catkin-tools
 echo "CATKIN WORKSPACE INIT"
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws
@@ -37,7 +37,7 @@ code --install-extension ms-vscode.cpptools
 code --install-extension ms-python.python
 code --install-extension ms-iot.vscode-ros
 
-sudo apt install ros-noetic-kobuki-msgs
+sudo apt install -y ros-noetic-kobuki-msgs
 
 cd ~/catkin_ws/src
 git clone https://git.ist.tugraz.at/courses/mobile_robots_public.git
